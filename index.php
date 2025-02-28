0467fa618f64e031fa0b244953223799755e4487
PakarTI <edy.unej@gmail.com>
2025-02-28 22:14:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -80,17 +80,3 @@
     </script>
 </body>
 </html>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Silaboma</title>
-</head>
-<body>
-    <h1>Welcome to Silaboma</h1>
-</body>
-</html>
